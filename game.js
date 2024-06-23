3f32eddf67b81979a28f317b2a749a30e77fe61a
Steccah <me@stecca.dev>
2024-06-23 17:16:13 +0200

    finished engine

--- game.js
+++ game.js
@@ -1,12 +1,11 @@
 class Cell {
-    constructor(value) {
+    constructor(value, solution) {
         this.value = value;
         this.state = 'normal'; // normal, enabled, disabled
+        this.solution = solution; // true, false
     }
 
-    reset() {
-        this.state = 'normal';
-    }
+    reset = () => this.state = 'normal';
 
     enable() {
         this.state = 'enabled';
@@ -24,9 +23,7 @@ class Cell {
         return this.state === 'enabled';
     }
 
-    isNormal() {
-        return this.state === 'normal';
-    }
+    isNormal = () => this.state === 'normal';
 }
 
 class Board {
@@ -34,10 +31,6 @@ class Board {
         this.dimension = dimension;
 
         this.cells = [];
-
-        for (let i = 0; i < dimension * dimension; i++) {
-            this.cells.push(new Cell(Math.floor(Math.random() * 9 + 1)));
-        }
 
         this.generate();
     }
@@ -53,7 +46,9 @@ class Board {
     getColSum(col) {
         let sum = 0;
         for (let row = 0; row < this.dimension; row++) {
-            sum += this.getCell(row, col).value;
+            if (this.getCell(row, col).isEnabled() || this.getCell(row, col).isNormal()) {
+                sum += this.getCell(row, col).value;
+            }
         }
         return sum;
     }
@@ -61,14 +56,38 @@ class Board {
     getRowSum(row) {
         let sum = 0;
         for (let col = 0; col < this.dimension; col++) {
-            sum += this.getCell(row, col).value;
+            if (this.getCell(row, col).isEnabled() || this.getCell(row, col).isNormal()) {
+                sum += this.getCell(row, col).value;
+            }
+        }
+        return sum;
+    }
+
+    getSolutionRowSum(row) {
+        let sum = 0;
+        for (let col = 0; col < this.dimension; col++) {
+            if (this.getCell(row, col).solution) {
+                sum += this.getCell(row, col).value;
+            }
+        }
+        return sum;
+    }
+
+    getSolutionColSum(col) {
+        let sum = 0;
+        for (let row = 0; row < this.dimension; row++) {
+            if (this.getCell(row, col).solution) {
+                sum += this.getCell(row, col).value;
+            }
         }
         return sum;
     }
 
     generate() {
-        for (let i = 0; i < this.cells.length; i++) {
-            this.cells[i] = new Cell(Math.floor(Math.random() * 9 + 1));
+        for (let i = 0; i < this.dimension * this.dimension; i++) {
+            let randValue = Math.floor(Math.random() * 9 + 1);
+            let randSolution = Math.random() < 0.6;
+            this.cells.push(new Cell(randValue, randSolution));
         }
     }
 
@@ -78,7 +97,47 @@ class Board {
 class Game {
     constructor(dimension) {
         this.board = new Board(dimension);
+        this.table = this.tableGenerator(dimension);
+        document.getElementById('game').appendChild(this.table);
         console.log(this.board);
+    }
+
+    tableGenerator(dimension) {
+        let table = document.createElement('table');
+        table.classList.add('table');
+        table.classList.add('is-bordered');
+
+
+        for (let i = 0; i < dimension; i++) {
+            let row = document.createElement('tr');
+            for (let j = 0; j < dimension; j++) {
+                let cell = document.createElement('td');
+                cell.id = 'cell-' + i + '-' + j;
+                cell.classList.add('cell');
+                cell.classList.add('is-clickable');
+                cell.onclick = () => this.toggleCell(i, j);
+                // cell.onclick = "game.toggleCell(" + i + "," + j + ")";
+                row.appendChild(cell);
+            }
+            let sumRow = document.createElement('td');
+            sumRow.id = 'sum-row-' + i;
+            sumRow.classList.add('cell');
+            sumRow.classList.add('is-info');
+            row.appendChild(sumRow);
+            table.appendChild(row);
+        }
+
+        let row = document.createElement('tr');
+        for (let i = 0; i < dimension; i++) {
+            let sumCol = document.createElement('td');
+            sumCol.id = 'sum-col-' + i;
+            sumCol.classList.add('cell');
+            sumCol.classList.add('is-info');
+            row.appendChild(sumCol);
+        }
+        table.appendChild(row);
+
+        return table;
     }
 
     start() {
@@ -91,8 +150,8 @@ class Game {
             }
         }
         for (let i = 0; i < this.board.dimension; i++) {
-            document.getElementById('sum-row-' + i).innerText = this.board.getRowSum(i);
-            document.getElementById('sum-col-' + i).innerText = this.board.getColSum(i);
+            document.getElementById('sum-row-' + i).innerText = this.board.getSolutionRowSum(i);
+            document.getElementById('sum-col-' + i).innerText = this.board.getSolutionColSum(i);
         }
     }
 
@@ -110,6 +169,35 @@ class Game {
             cell.enable();
             element.classList.remove('is-danger');
             element.classList.add('is-success');
+        }
+    }
+
+    check() {
+        for (let i = 0; i < this.board.dimension; i++) {
+            let sumRow = this.board.getRowSum(i);
+            let sumCol = this.board.getColSum(i);
+            if (sumRow == this.board.getSolutionRowSum(i)) {
+                document.getElementById('sum-row-' + i).classList.add('is-success');
+            } else {
+                document.getElementById('sum-row-' + i).classList.remove('is-success');
+            }
+            if (sumCol == this.board.getSolutionColSum(i)) {
+                document.getElementById('sum-col-' + i).classList.add('is-success');
+            } else {
+                document.getElementById('sum-col-' + i).classList.remove('is-success');
+            }
+        }
+
+        let win = true;
+        for (let i = 0; i < this.board.dimension; i++) {
+            if (!document.getElementById('sum-row-' + i).classList.contains('is-success')
+                || !document.getElementById('sum-col-' + i).classList.contains('is-success')) {
+                win = false;
+            }
+        }
+
+        if (win) {
+            alert('You win!');
         }
     }
 }
